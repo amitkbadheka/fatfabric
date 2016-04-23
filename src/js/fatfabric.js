7088a6fdc054e430e0b7cfff180594e773c687e4
akshayda04 <akshay.agrawal@pykih.com>
2016-04-23 14:23:31 +0530

    added tabs for different fabrics

--- src/js/fatfabric.js
+++ src/js/fatfabric.js
@@ -1,3 +1,56 @@
+var tabs = $(".tab"),
+  i,
+  technbiz_threads = ["Techcrunch","HackerNews","DataTau","akshay"],
+  design_threads = ["DesignerNews","ProductHunt","TheNextWeb"],
+  dz_threads = ["DesignMilk","Behance","Dribbble"],
+  custom_threads = [];
+  number_of_tech = technbiz_threads.length,
+  number_of_design = design_threads.length,
+  number_of_dz = dz_threads.length,
+  tech_string = "",
+  design_string = "",
+  dz_string = "",
+  tech_string = "";
+
+for(i = 0; i < number_of_tech; i++) {
+  tech_string+= "<div class = 'container' id = '"+technbiz_threads[i]+"'>"+technbiz_threads[i]+" </div>";
+}
+document.getElementById("business").innerHTML = tech_string;
+for(i = 0; i < number_of_design; i++) {
+  design_string += "<div class = 'container' id = '"+design_threads[i]+"'>"+design_threads[i]+"</div>";
+}
+document.getElementById("design").innerHTML = design_string;
+
+for(i = 0; i < number_of_dz; i++) {
+  dz_string += "<div class = 'container' id = '"+dz_threads[i]+"'>"+dz_threads[i]+"</div>";
+}
+document.getElementById("design_plus").innerHTML = dz_string
+
+var business = document.createElement("div");
+// for(i = 0; i < number_of_feeds; i++) {
+//  var div = document.createElement("div");
+
+// }
+
+tabs.on("click" , function(e) {
+  var number_of_tabs = tabs.length;
+  for(var i = 0; i < number_of_tabs; i++) {
+    if($(tabs[i]).hasClass("is-tab-selected")) {
+      var tab = $(tabs[i]),
+        tab_child = tab.data("name");
+        $("#"+tab_child).css("display","none");
+      tab.removeClass("is-tab-selected");
+      tab.addClass("not-selected");
+      break;
+    }
+  }
+  var target = $(e.target)
+  , child = target.data("name");
+  target.removeClass("not-selected");
+  target.addClass("is-tab-selected");
+  $("#"+child).css("display","block");
+});
+
 var fdb = new ForerunnerDB();
 
 var db = fdb.db("fatfabric");
@@ -29,7 +82,7 @@ function initialize() {
   var all_feeds = feeds.find();
   for(var j = 0; j<all_feeds.length;j++){
     var feed = new google.feeds.Feed(all_feeds[j].feed);
-    container = document.getElementById("feed"),
+    container = document.getElementById("design"),
     html_string = "<div>";
     feed.load(function(result) {
       if (!result.error) {
@@ -46,3 +99,4 @@ function initialize() {
 
 }
 google.setOnLoadCallback(initialize);
+
